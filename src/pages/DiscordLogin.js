384e39cba468ac9cba7b8347ce84237bc104d04e
Tiffani Hess <hesstjune@gmail.com>
2020-08-19 16:10:47 -0500

    added user to global context when signing in with discord

--- src/pages/DiscordLogin.js
+++ src/pages/DiscordLogin.js
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import getParameterByName from "../utilities/getParameterByName";
 import { discordSignup } from "../api/discordApi";
+import { UserContext } from "../context/UserContext";
 
 import { Redirect } from "react-router-dom";
 
 const DiscordLogin = () => {
   const [loggedIn, setLoggedIn] = useState(false);
-
+  const { setUser } = useContext(UserContext);
   //TODO: handle user trying to login with discord again after already logged in with discord
   useEffect(() => {
     const state = getParameterByName("state");
@@ -14,11 +15,13 @@ const DiscordLogin = () => {
 
     const postDiscordSignup = async () => {
       const user = await discordSignup(code, state);
-      if (user.id) setLoggedIn(true);
-      //TODO: add logged in to global state
+      if (user.id) {
+        setLoggedIn(true);
+        setUser(user);
+      }
     };
     postDiscordSignup();
-  }, []);
+  }, [setUser]);
 
   return (
     <div>{loggedIn ? <Redirect to="/" /> : <span> Loading... </span>}</div>
