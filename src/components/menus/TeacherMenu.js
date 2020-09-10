b0de77825b9aea55647bf7c6df42c57551771b31
Tiffani Hess <hesstjune@gmail.com>
2020-09-10 12:45:43 -0500

    removed error component from teacherMenu, throwing in comment and question lists

--- src/components/menus/TeacherMenu.js
+++ src/components/menus/TeacherMenu.js
@@ -18,13 +18,6 @@ const TeacherMenu = ({
   const { error, setError } = useContext(ErrorContext);
   const history = useHistory();
 
-  // const [answerId, setAnswerId] = useState(undefined);
-  // // const [error, setError] = useState(undefined);
-
-  // const replaceAnswer = async (id) => {
-  //   //figure out how to handle situation where already have answer and want to replace, using state won't persist on sign out or reload
-  // };
-
   const deleteUserQuestion = async () => {
     if (window.confirm("Are you sure you want to delete this question?")) {
       const deletedQuestion = await deleteQuestion(question.id);
@@ -46,33 +39,25 @@ const TeacherMenu = ({
   if (question) {
     return (
       <Fragment>
-        {error ? (
-          <Error />
-        ) : (
-          <StyledTeacherMenu>
-            <StyledParagraph onClick={deleteUserQuestion}>
-              Remove Post
-            </StyledParagraph>
-          </StyledTeacherMenu>
-        )}
+        <StyledTeacherMenu>
+          <StyledParagraph onClick={deleteUserQuestion}>
+            Remove Post
+          </StyledParagraph>
+        </StyledTeacherMenu>
       </Fragment>
     );
   }
   return (
     <Fragment>
-      {error ? (
-        <Error />
-      ) : (
-        <StyledTeacherMenu>
-          <StyledParagraph onClick={() => chooseAnswer(comment)}>
-            Promote as Answer
-          </StyledParagraph>
-          <hr />
-          <StyledParagraph onClick={() => deleteUserComment(comment)}>
-            Remove Post
-          </StyledParagraph>
-        </StyledTeacherMenu>
-      )}
+      <StyledTeacherMenu>
+        <StyledParagraph onClick={() => chooseAnswer(comment)}>
+          Promote as Answer
+        </StyledParagraph>
+        <hr />
+        <StyledParagraph onClick={() => deleteUserComment(comment)}>
+          Remove Post
+        </StyledParagraph>
+      </StyledTeacherMenu>
     </Fragment>
   );
 };
