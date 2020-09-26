2668e6d28875596984855d44d46357b8ea605d32
Tiffani Hess <hesstjune@gmail.com>
2020-09-26 14:19:27 -0500

    styling changes: added Styled Layout to style the challenge board header in the nav, adjust nav display flex to align items center so header wasn't off kilter

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import UserMenu from "../menus/UserMenu";
 import { Link } from "react-router-dom";
+import { StyledNavHeader } from "./StyledLayout";
 
 const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,11 +16,11 @@ const Nav = () => {
       <Link to="/challenge">
         <i className="fas fa-chess-knight fa-lg"></i>
       </Link>
-      <h3>
-        <Link to="/challenge" style={{ color: "#809bff" }}>
-          CHALLENGE BOARD
-        </Link>
-      </h3>
+
+      <StyledNavHeader to="/challenge">
+        CHALLENGE BOARD
+        </StyledNavHeader>
+
       <i
         id="user-menu-toggle"
         className="fas fa-user-circle fa-2x"
---
+++ src/components/layout/StyledLayout.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const StyledNavHeader = styled(Link)`
+
+  color: var(--title-text);
+  font-family: "Roboto Slab", serif;
+  font-weight: 500;
+font-size: 1.1rem;
+`;
+
+
+export { StyledNavHeader };
