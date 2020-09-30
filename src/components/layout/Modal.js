50df2f7cb4c66384e825ea43e84f48eea2c91b11
Tiffani Hess <hesstjune@gmail.com>
2020-09-29 19:49:56 -0500

    added handleClickOutside function and state to modal, to close if click outside

--- src/components/layout/Modal.js
+++ src/components/layout/Modal.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Modal = (props) => {
+  const [modalOpen, setModalOpen] = useState(true);
+
+  const handleClickOutside = () => {
+    setModalOpen(false);
+  };
+
   return (
-    <div className="modal">
-      <div className="modal-content">{props.children}</div>
-    </div>
+    modalOpen && (
+      <div className="modal" onClick={handleClickOutside}>
+        <div className="modal-content">{props.children}</div>
+      </div>
+    )
   );
 };
 
